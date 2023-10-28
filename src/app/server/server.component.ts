c4691d5207e8fcb28eaff4f3d92b22ae740b1720
Alexandre Bessard <alexandre.bessard@gmail.com>
2023-10-28 18:50:49 -0400

    Styling elements

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,15 +2,29 @@ import {Component} from "@angular/core";
 // Decorator
 @Component({ // javascript object
   selector: "app-server",
-  templateUrl: "./server.component.html" // need a relative path
+  templateUrl: "./server.component.html", // need a relative path
+  styles: [`
+    .online {
+      color: pink;
+    }
+  `]
 })
 // Component is like a typescript class
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = "offline";
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+  }
+
 
   getServerStatus(): string {
     return this.serverStatus;
   }
+
+  getColor() {
+    return this.serverStatus === "online" ? "green" : "red";
+  }
+
 }
